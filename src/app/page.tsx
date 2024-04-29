216afcab9c495f5bd8fe06a6ecaa1ca3cdfc30d7
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-04-30 00:30:27 +0300

    Completed nike product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,214 @@
 import Image from "next/image";
+import { Container, Flex, Button, Link, Strong, Box } from "@radix-ui/themes";
+import {
+  ChevronRightIcon,
+  ChevronLeftIcon,
+  StarFilledIcon,
+  HeartIcon
+} from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen p-24">
+      <Container>
+        <Flex direction="row" justify="between">
+          <div className="photo">
+            <Flex direction="row" gap="4" justify="between" align="stretch">
+              <Flex direction="column" gap="2">
+                <Image
+                  className="rounded shadow-md"
+                  src="/image.png"
+                  alt="Thumbnail 1"
+                  height={70}
+                  width={70}
+                />
+                <Image
+                  className="rounded shadow-md"
+                  src="/image2.png"
+                  alt="Thumbnail 2"
+                  height={70}
+                  width={70}
+                />
+                <Image
+                  className="rounded shadow-md"
+                  src="/image3.png"
+                  alt="Thumbnail 3"
+                  height={70}
+                  width={70}
+                />
+              </Flex>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div
+                className="large-image-container"
+                style={{ position: "relative" }}
+              >
+                <Image
+                  className="rounded shadow-lg"
+                  src="/image-large.png"
+                  alt="Large Image"
+                  height={535}
+                  width={670}
+                />
+                <Button
+                  size="3"
+                  radius="full"
+                  style={{
+                    position: "absolute",
+                    top: "10px",
+                    left: "10px",
+                    backgroundColor: "white",
+                    color: "black",
+                  }}
+                >
+                  <StarFilledIcon /> Highly Rated
+                </Button>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                <Flex
+                  direction="row"
+                  gap="2"
+                  style={{
+                    position: "absolute",
+                    bottom: "10px",
+                    right: "10px",
+                  }}
+                >
+                  <Button
+                    size="3"
+                    radius="full"
+                    style={{
+                      backgroundColor: "white",
+                      color: "black",
+                    }}
+                    className="bg-black text-white"
+                  >
+                    <ChevronLeftIcon />
+                  </Button>
+                  <Button
+                    size="3"
+                    radius="full"
+                    style={{
+                      backgroundColor: "white",
+                      color: "black",
+                    }}
+                    className="bg-black text-white"
+                  >
+                    <ChevronRightIcon />
+                  </Button>
+                </Flex>
+              </div>
+            </Flex>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold text-gray-900">
+              Air Jordan 1 Retro High OG
+            </h1>
+            <p className="text-base font-bold text-gray-900 mb-4">
+              Men&apos;s Shoes
+            </p>
+            <p className="text-lg font-bold text-gray-900 mb-4">$180</p>
+            <div className="choise-model mb-7">
+              <Flex direction="row" gap="1">
+                <Image
+                  className="rounded shadow-md"
+                  src="/image.png"
+                  alt="air force"
+                  height={70}
+                  width={70}
+                />
+                <Image
+                  className="rounded shadow-md"
+                  src="/image3.png"
+                  alt="air force"
+                  height={70}
+                  width={70}
+                />
+                <Image
+                  className="rounded shadow-md"
+                  src="/image2.png"
+                  alt="air force"
+                  height={70}
+                  width={70}
+                />
+              </Flex>
+            </div>
+            <div className="select-size mb-6">
+              <Flex direction="row" justify="between">
+                <h3>Select Size</h3>
+                <h3>Size Guide</h3>
+              </Flex>
+              <Flex direction="column" gap="2">
+                {Array.from({ length: 5 }, (_, i) => (
+                  <Flex key={i} direction="row" gap="1" justify="between">
+                    <Button size="4" radius="small" variant="outline">
+                      M {3.5 + i} / W{5 + i}
+                    </Button>
+                    <Button size="4" radius="small" variant="outline">
+                      M {3.5 + i} / W{5 + i}
+                    </Button>
+                  </Flex>
+                ))}
+              </Flex>
+            </div>
+            <div className="buy-section mb-6">
+              <div>
+                <p className="p-5 text-center w-80">
+                  4 interest-free payments of $45.00 with{" "}
+                  <Strong>Klarna.</Strong>{" "}
+                  <Link href="#" underline="always">
+                    Learn More
+                  </Link>
+                </p>
+                <Button
+                  style={{
+                    backgroundColor: "black",
+                    color: "white",
+                    height: "55px",
+                    width: "100%",
+                    marginBottom: "0.75rem",
+                  }}
+                  radius="full"
+                >
+                  Add to bag
+                </Button>
+                <Button
+                  style={{
+                    backgroundColor: "white",
+                    color: "Black",
+                    height: "55px",
+                    width: "100%",
+                    marginBottom: "0.75rem",
+                    border: "1px solid black",
+                  }}
+                  radius="full"
+                >
+                  Favorite <HeartIcon />
+                </Button>
+                <p className="p-5 text-center w-80">
+                  This product is excluded from site promotions and discounts.
+                </p>
+                <h4 className="font-semibold">Shipping</h4>
+                <p className="mb-4">
+                  You&apos;ll see our shipping options at checkout
+                </p>
+                <h4>Free Pickup</h4>
+                <Link href="#" underline="always">
+                  Find a Store
+                </Link>
+                <p className="w-80 mt-5 mb-5">
+                  The Air Jordan 1 Retro High remakes the classic sneaker,
+                  giving you a fresh look with a familiar feel. Premium
+                  materials with new colors and textures give modern expression
+                  to an all-time favorite.
+                </p>
+                <ul>
+                  <li>Shown: White/Team Red</li>
+                  <li>Style: DZ5485-160</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </Flex>
+      </Container>
     </main>
   );
 }
